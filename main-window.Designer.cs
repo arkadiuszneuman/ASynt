390aab2ad92334ae89c2ab23ac2147d437e839e6
unknown <Arek@.(none)>
2011-05-22 09:41:18 +0200

    Almost-working keyboard
    Signed-off-by: unknown <Arek@.(none)>

--- main-window.Designer.cs
+++ main-window.Designer.cs
@@ -28,22 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.effectsGB = new System.Windows.Forms.GroupBox();
+            this.soundTypeGB = new System.Windows.Forms.GroupBox();
+            this.globalMuteCB = new System.Windows.Forms.CheckBox();
+            this.globalVolumeSlider = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.globalVolumeSlider)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(670, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // effectsGB
+            // 
+            this.effectsGB.Location = new System.Drawing.Point(12, 12);
+            this.effectsGB.Name = "effectsGB";
+            this.effectsGB.Size = new System.Drawing.Size(200, 100);
+            this.effectsGB.TabIndex = 1;
+            this.effectsGB.TabStop = false;
+            this.effectsGB.Text = "Efekty";
+            // 
+            // soundTypeGB
+            // 
+            this.soundTypeGB.Location = new System.Drawing.Point(649, 12);
+            this.soundTypeGB.Name = "soundTypeGB";
+            this.soundTypeGB.Size = new System.Drawing.Size(200, 100);
+            this.soundTypeGB.TabIndex = 2;
+            this.soundTypeGB.TabStop = false;
+            this.soundTypeGB.Text = "Rodzaj dźwięku";
+            // 
+            // globalMuteCB
+            // 
+            this.globalMuteCB.AutoSize = true;
+            this.globalMuteCB.Location = new System.Drawing.Point(374, 65);
+            this.globalMuteCB.Name = "globalMuteCB";
+            this.globalMuteCB.Size = new System.Drawing.Size(60, 17);
+            this.globalMuteCB.TabIndex = 3;
+            this.globalMuteCB.Text = "Wycisz";
+            this.globalMuteCB.UseVisualStyleBackColor = true;
+            // 
+            // globalVolumeSlider
+            // 
+            this.globalVolumeSlider.Location = new System.Drawing.Point(275, 12);
+            this.globalVolumeSlider.Name = "globalVolumeSlider";
+            this.globalVolumeSlider.Size = new System.Drawing.Size(283, 45);
+            this.globalVolumeSlider.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(566, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(471, 312);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(784, 158);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(861, 359);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.globalVolumeSlider);
+            this.Controls.Add(this.globalMuteCB);
+            this.Controls.Add(this.soundTypeGB);
+            this.Controls.Add(this.effectsGB);
+            this.Controls.Add(this.button1);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "ASynt - Awesome Syntezator";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.globalVolumeSlider)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox effectsGB;
+        private System.Windows.Forms.GroupBox soundTypeGB;
+        private System.Windows.Forms.CheckBox globalMuteCB;
+        private System.Windows.Forms.TrackBar globalVolumeSlider;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- main-window.cs
+++ main-window.cs
@@ -6,6 +6,7 @@ using System.Windows.Forms;
 using ASynt.Player;
 using System.Drawing;
 using ASynt.Keyboard;
+using Un4seen.Bass;
 
 namespace ASynt
 {
@@ -18,7 +19,7 @@ namespace ASynt
         {
             InitializeComponent();
             //key = new Key(new System.Drawing.Point(0, 0), 1000);
-            keyboard = new Keyboard.Keyboard(this, new Point(0, 0));
+            keyboard = new Keyboard.Keyboard(this, new Point(50, 150));
         }
 
         private void MainForm_KeyDown(object sender, KeyEventArgs e)
@@ -27,19 +28,79 @@ namespace ASynt
 
             if (e.KeyCode == Keys.A)
             {
-                sound.ChangeFrequency(60000);
+                sound.ChangeFrequency(440);
                 //player.Play(sound);
             }
             else if (e.KeyCode == Keys.S)
             {
-                sound.ChangeFrequency(80000);
+                sound.ChangeFrequency(440);
                 //player.Play(sound);
             }
             else if (e.KeyCode == Keys.D)
             {
-                sound.ChangeFrequency(100000);
+                sound.ChangeFrequency(440);
                 //player.Play(sound);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            // create the sample
+            int sample = Bass.BASS_SampleCreate(44100, 44100, 2, 1, BASSFlag.BASS_SAMPLE_LOOP | BASSFlag.BASS_SAMPLE_OVER_POS);
+            // the data buffer (5000 byte = 128 Int16)
+            short[] data = new short[22050];
+            // create the sine wave
+            for (int a = 0; a < 22050; a++)
+                data[a] = (short)((0.25 * short.MaxValue) * Math.Sin(Math.Sin((2 * Math.PI * a * 440) / 44100)) + (0.25 * short.MaxValue) * Math.Abs(Math.Tan(2*Math.PI*a*440/44100))); //32767
+            // set the sample's data 
+            Bass.BASS_SampleSetData(sample, data);
+            // get a sample channel
+            int channel = Bass.BASS_SampleGetChannel(sample, false);
+            // play it
+           Bass.BASS_ChannelPlay(channel, false);
+           Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            // create the sample
+            int sample = Bass.BASS_SampleCreate(44100, 44100, 2, 1, BASSFlag.BASS_SAMPLE_LOOP | BASSFlag.BASS_SAMPLE_OVER_POS);
+            // the data buffer (5000 byte = 128 Int16)
+            short[] data = new short[22050];
+            // create the sine wave
+            for (int a = 0; a < 22050; a++)
+                data[a] = (short)((0.25 * short.MaxValue) * Math.Sin(Math.Sin((2 * Math.PI * a * 880) / 44100)) + (0.25 * short.MaxValue) * Math.Abs(Math.Tan(2 * Math.PI * a * 880 / 44100))); //32767
+            // set the sample's data 
+            Bass.BASS_SampleSetData(sample, data);
+            // get a sample channel
+            int channel = Bass.BASS_SampleGetChannel(sample, false);
+            // play it
+            Bass.BASS_ChannelPlay(channel, false);
+            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            // create the sample
+            int sample = Bass.BASS_SampleCreate(44100, 44100, 2, 1, BASSFlag.BASS_SAMPLE_LOOP | BASSFlag.BASS_SAMPLE_OVER_POS);
+            // the data buffer (5000 byte = 128 Int16)
+            short[] data = new short[22050];
+            // create the sine wave
+            for (int a = 0; a < 22050; a++)
+                data[a] = (short)((0.25 * short.MaxValue) * Math.Sin(Math.Sin((2 * Math.PI * a * 1300) / 44100)) * (0.25 * short.MaxValue) * Math.Abs(Math.Tan(2 * Math.PI * a * 1300 / 44100))); //32767
+            // set the sample's data 
+            Bass.BASS_SampleSetData(sample, data);
+            // get a sample channel
+            int channel = Bass.BASS_SampleGetChannel(sample, false);
+            // play it
+            Bass.BASS_ChannelPlay(channel, false);
+            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
         }
     }
 }
