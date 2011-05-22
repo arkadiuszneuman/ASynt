72284eb0014e6adb484db61a782271eb77cf155e
Maciej Płaza <plaza.maciej@gmail.com>
2011-05-22 10:25:13 +0200

    12 buttons for sample playing
    Signed-off-by: Maciej Płaza <plaza.maciej@gmail.com>

--- main-window.Designer.cs
+++ main-window.Designer.cs
@@ -31,20 +31,31 @@
             this.button1 = new System.Windows.Forms.Button();
             this.effectsGB = new System.Windows.Forms.GroupBox();
             this.soundTypeGB = new System.Windows.Forms.GroupBox();
+            this.soundGeneratorB = new System.Windows.Forms.Button();
             this.globalMuteCB = new System.Windows.Forms.CheckBox();
             this.globalVolumeSlider = new System.Windows.Forms.TrackBar();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.soundTypeGB.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.globalVolumeSlider)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(670, 312);
+            this.button1.Location = new System.Drawing.Point(676, 310);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(50, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -59,12 +70,23 @@
             // 
             // soundTypeGB
             // 
+            this.soundTypeGB.Controls.Add(this.soundGeneratorB);
             this.soundTypeGB.Location = new System.Drawing.Point(649, 12);
             this.soundTypeGB.Name = "soundTypeGB";
             this.soundTypeGB.Size = new System.Drawing.Size(200, 100);
             this.soundTypeGB.TabIndex = 2;
             this.soundTypeGB.TabStop = false;
             this.soundTypeGB.Text = "Rodzaj dźwięku";
+            // 
+            // soundGeneratorB
+            // 
+            this.soundGeneratorB.Location = new System.Drawing.Point(6, 71);
+            this.soundGeneratorB.Name = "soundGeneratorB";
+            this.soundGeneratorB.Size = new System.Drawing.Size(188, 23);
+            this.soundGeneratorB.TabIndex = 0;
+            this.soundGeneratorB.Text = "Generuj";
+            this.soundGeneratorB.UseVisualStyleBackColor = true;
+            this.soundGeneratorB.Click += new System.EventHandler(this.soundGenerator);
             // 
             // globalMuteCB
             // 
@@ -85,23 +107,113 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(566, 312);
+            this.button2.Location = new System.Drawing.Point(620, 310);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(50, 23);
             this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
+            this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(471, 312);
+            this.button3.Location = new System.Drawing.Point(564, 310);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(50, 23);
             this.button3.TabIndex = 6;
-            this.button3.Text = "button3";
+            this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(508, 310);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(452, 310);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(396, 310);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(340, 310);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(284, 310);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 23);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(228, 310);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 23);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(172, 310);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(50, 23);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(116, 310);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(50, 23);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "11";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(60, 310);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(50, 23);
+            this.button12.TabIndex = 15;
+            this.button12.Text = "12";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // MainForm
             // 
@@ -109,6 +221,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(861, 359);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.globalVolumeSlider);
@@ -119,6 +240,7 @@
             this.Name = "MainForm";
             this.Text = "ASynt - Awesome Syntezator";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.soundTypeGB.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.globalVolumeSlider)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -134,6 +256,16 @@
         private System.Windows.Forms.TrackBar globalVolumeSlider;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button soundGeneratorB;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
     }
 }
 
