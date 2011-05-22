2bc4bfa2367b31347c4c5ee6baaf8c92272739f3
Maciej Płaza <plaza.maciej@gmail.com>
2011-05-22 15:10:43 +0200

    New functions
    Signed-off-by: Maciej Płaza <plaza.maciej@gmail.com>

--- SyntMath.cs
+++ SyntMath.cs
@@ -7,12 +7,83 @@ namespace ASynt
 {
     public class SyntMath
     {
-        public static short[] Sinus(double ampl, int freq)
+        public static short[] Sinus(double ampl, int freq, int samplesCount)
         {
-            short[] data = new short[44100];
-            for (int i = 0; i < 44100; ++i)
+            short[] data = new short[samplesCount];
+            for (int i = 0; i < samplesCount; ++i)
             {
                 data[i] = (short)((ampl * short.MaxValue) * Math.Sin((2 * Math.PI * i * freq) / 44100));
+            }
+            return data;
+        }
+
+        public static short[] AbsSinus(double ampl, int freq, int samplesCount)
+        {
+            short[] data = new short[samplesCount];
+            for (int i = 0; i < samplesCount; ++i)
+            {
+                data[i] = (short)((ampl * short.MaxValue) * Math.Abs(Math.Sin((2 * Math.PI * i * freq) / 44100)));
+            }
+            return data;
+        }
+
+        public static short[] Cosinus(double ampl, int freq, int samplesCount)
+        {
+            short[] data = new short[samplesCount];
+            for (int i = 0; i < samplesCount; ++i)
+            {
+                data[i] = (short)((ampl * short.MaxValue) * Math.Cos((2 * Math.PI * i * freq) / 44100));
+            }
+            return data;
+        }
+
+        public static short[] AbsCosinus(double ampl, int freq, int samplesCount)
+        {
+            short[] data = new short[samplesCount];
+            for (int i = 0; i < samplesCount; ++i)
+            {
+                data[i] = (short)((ampl * short.MaxValue) * Math.Abs(Math.Cos((2 * Math.PI * i * freq) / 44100)));
+            }
+            return data;
+        }
+
+        public static short[] Tangens(double ampl, int freq, int samplesCount)
+        {
+            short[] data = new short[samplesCount];
+            for (int i = 0; i < samplesCount; ++i)
+            {
+                data[i] = (short)((ampl * short.MaxValue) * Math.Tan((2 * Math.PI * i * freq) / 44100));
+            }
+            return data;
+        }
+
+        public static short[] AbsTangens(double ampl, int freq, int samplesCount)
+        {
+            short[] data = new short[samplesCount];
+            for (int i = 0; i < samplesCount; ++i)
+            {
+                data[i] = (short)((ampl * short.MaxValue) * Math.Abs(Math.Tan((2 * Math.PI * i * freq) / 44100)));
+            }
+            return data;
+        }
+
+        public static short[] Square(double ampl, int freq, int samplesCount)
+        {
+            short[] data = new short[samplesCount];
+            for (int i = 0; i < samplesCount; ++i)
+            {
+                data[i] = (short)((ampl * short.MaxValue) * Math.Sign(Math.Sin((2 * Math.PI * i * freq) / 44100)));
+            }
+            return data;
+        }
+
+        public static short[] WhiteNoise(double ampl, int freq, int samplesCount)
+        {
+            Random random = new Random();
+            short[] data = new short[samplesCount];
+            for (int i = 0; i < samplesCount; ++i)
+            {
+                data[i] = (short)((ampl * short.MaxValue) * (2 * random.Next(short.MaxValue) / short.MinValue - 1));
             }
             return data;
         }
--- SyntWave.cs
+++ SyntWave.cs
@@ -7,5 +7,15 @@ namespace ASynt
 {
     public struct SyntWave
     {
+        public int signal;
+        public int from;
+        public int to;
+
+        public SyntWave(int Signal, int From, int To)
+        {
+            this.signal = Signal;
+            this.from = From;
+            this.to = To;            
+        }
     }
 }
