dc4ddad3e4976509d57a2bd5600185de0c4ea5dd
Maciej Płaza <plaza.maciej@gmail.com>
2011-05-22 10:25:44 +0200

    Class for BASS Samples
    Signed-off-by: Maciej Płaza <plaza.maciej@gmail.com>

---
+++ Sample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Un4seen.Bass;
+
+namespace ASynt 
+{
+    public class Sample
+    {
+        public int sampleHandle;
+        private short[] data = new short[44100]; // Sample rate = 44100; 44100 bajtów = 22050 INT16
+        
+        public Sample()
+	    {
+            sampleHandle = Bass.BASS_SampleCreate(88200, 44100, 2, 1, BASSFlag.BASS_SAMPLE_OVER_POS);
+	    }
+
+        public void CreateSound(int freq)
+        {
+            data = SyntMath.Sinus(0.25, freq);
+
+            Bass.BASS_SampleSetData(sampleHandle, data);
+        }
+    }
+}
---
+++ SampleSounds.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Un4seen.Bass;
+
+namespace ASynt
+{
+    public class SampleSounds
+    {
+        private Sample[] samples;
+
+        public SampleSounds()
+        {
+            samples = new Sample[12];
+            CreateSamples();
+        }
+
+        private void CreateSamples()
+        {
+            for (int i = 0; i < 12; ++i)
+            {
+                samples[i] = new Sample();
+                samples[i].CreateSound((i + 1) * 100);
+            }
+        }
+
+        public int GetChannelHandle(int id) 
+        {
+            return Bass.BASS_SampleGetChannel(samples[id].sampleHandle, false);
+        }
+    }
+}
