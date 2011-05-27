40055987395853b5752629e78ec807dc99f7bbf8
Maciej Płaza <plaza.maciej@gmail.com>
2011-05-27 15:44:01 +0200

    Cleaning
    Signed-off-by: Maciej Płaza <plaza.maciej@gmail.com>

--- main-window.Designer.cs
+++ main-window.Designer.cs
@@ -28,38 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.effectsGB = new System.Windows.Forms.GroupBox();
             this.buttonEcho = new System.Windows.Forms.Button();
             this.soundTypeGB = new System.Windows.Forms.GroupBox();
             this.soundGeneratorB = new System.Windows.Forms.Button();
             this.globalMuteCB = new System.Windows.Forms.CheckBox();
             this.globalVolumeSlider = new System.Windows.Forms.TrackBar();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
-            this.button12 = new System.Windows.Forms.Button();
             this.effectsGB.SuspendLayout();
             this.soundTypeGB.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.globalVolumeSlider)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(628, 310);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(50, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // effectsGB
             // 
@@ -118,138 +96,16 @@
             this.globalVolumeSlider.Size = new System.Drawing.Size(198, 45);
             this.globalVolumeSlider.TabIndex = 4;
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(572, 310);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(50, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(516, 310);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(50, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(460, 310);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(50, 23);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(404, 310);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(50, 23);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "5";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(348, 310);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(50, 23);
-            this.button6.TabIndex = 9;
-            this.button6.Text = "6";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(292, 310);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(50, 23);
-            this.button7.TabIndex = 10;
-            this.button7.Text = "7";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(236, 310);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(50, 23);
-            this.button8.TabIndex = 11;
-            this.button8.Text = "8";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(180, 310);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(50, 23);
-            this.button9.TabIndex = 12;
-            this.button9.Text = "9";
-            this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(124, 310);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(50, 23);
-            this.button10.TabIndex = 13;
-            this.button10.Text = "10";
-            this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.button10_Click);
-            // 
-            // button11
-            // 
-            this.button11.Location = new System.Drawing.Point(68, 310);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(50, 23);
-            this.button11.TabIndex = 14;
-            this.button11.Text = "11";
-            this.button11.UseVisualStyleBackColor = true;
-            this.button11.Click += new System.EventHandler(this.button11_Click);
-            // 
-            // button12
-            // 
-            this.button12.Location = new System.Drawing.Point(12, 310);
-            this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(50, 23);
-            this.button12.TabIndex = 15;
-            this.button12.Text = "12";
-            this.button12.UseVisualStyleBackColor = true;
-            this.button12.Click += new System.EventHandler(this.button12_Click);
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(684, 359);
-            this.Controls.Add(this.button12);
-            this.Controls.Add(this.button11);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(579, 275);
             this.Controls.Add(this.globalVolumeSlider);
             this.Controls.Add(this.globalMuteCB);
             this.Controls.Add(this.soundTypeGB);
             this.Controls.Add(this.effectsGB);
-            this.Controls.Add(this.button1);
             this.KeyPreview = true;
             this.Name = "MainForm";
             this.Text = "ASynt - Awesome Syntezator";
@@ -263,23 +119,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox effectsGB;
         private System.Windows.Forms.GroupBox soundTypeGB;
         private System.Windows.Forms.CheckBox globalMuteCB;
         private System.Windows.Forms.TrackBar globalVolumeSlider;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button soundGeneratorB;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button11;
-        private System.Windows.Forms.Button button12;
         private System.Windows.Forms.Button buttonEcho;
     }
 }
--- main-window.cs
+++ main-window.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.IO;
 using System.Windows.Forms;
 using ASynt.Player;
 using System.Drawing;
 using ASynt.Keyboard;
 using Un4seen.Bass;
+using System.Runtime.InteropServices;
 
 namespace ASynt
 {
@@ -14,104 +16,19 @@ namespace ASynt
     {
         //SoundPlayer player = new SoundPlayer();
         Keyboard.Keyboard keyboard;
-        SampleSounds sampleSounds;
+        private GCHandle _hGCFile;
+
 
         public MainForm()
         {
             InitializeComponent();
             //key = new Key(new System.Drawing.Point(0, 0), 1000);
             keyboard = new Keyboard.Keyboard(this, new Point(20, 20));
-            sampleSounds = new SampleSounds();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //int channel = sampleSounds.GetChannelHandle(0);
-           // Bass.BASS_ChannelPlay(channel, false);
-            Bass.BASS_ChannelPlay(sampleSounds.samples[0].channelHandle, true);
-            //Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 10000);
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            int channel = sampleSounds.GetChannelHandle(1);
-            Bass.BASS_ChannelPlay(channel, false);
-            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 10000);
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            int channel = sampleSounds.GetChannelHandle(2);
-            Bass.BASS_ChannelPlay(channel, false);
-            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            int channel = sampleSounds.GetChannelHandle(3);
-            Bass.BASS_ChannelPlay(channel, false);
-            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            int channel = sampleSounds.GetChannelHandle(4);
-            Bass.BASS_ChannelPlay(channel, false);
-            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            int channel = sampleSounds.GetChannelHandle(5);
-            Bass.BASS_ChannelPlay(channel, false);
-            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            int channel = sampleSounds.GetChannelHandle(6);
-            Bass.BASS_ChannelPlay(channel, false);
-            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            int channel = sampleSounds.GetChannelHandle(7);
-            Bass.BASS_ChannelPlay(channel, false);
-            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            int channel = sampleSounds.GetChannelHandle(8);
-            Bass.BASS_ChannelPlay(channel, false);
-            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
-        }
-
-        private void button10_Click(object sender, EventArgs e)
-        {
-            int channel = sampleSounds.GetChannelHandle(9);
-            Bass.BASS_ChannelPlay(channel, false);
-            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
-        }
-
-        private void button11_Click(object sender, EventArgs e)
-        {
-            int channel = sampleSounds.GetChannelHandle(10);
-            Bass.BASS_ChannelPlay(channel, false);
-            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
-        }
-
-        private void button12_Click(object sender, EventArgs e)
-        {
-            int channel = sampleSounds.GetChannelHandle(11);
-            Bass.BASS_ChannelPlay(channel, false);
-            Bass.BASS_ChannelSlideAttribute(channel, BASSAttribute.BASS_ATTRIB_VOL, 0, 1000);
         }
 
         private void soundGenerator(object sender, EventArgs e)
         {
-            SoundGenerator sg = new SoundGenerator(sampleSounds, keyboard);
+            SoundGenerator sg = new SoundGenerator(keyboard);
             sg.ShowDialog();
         }
 
